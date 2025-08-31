2f423b3e52b9c45d5c6057b6c1450147d8b85096
Timon <timon-albrecht@outlook.de>
2025-08-31 23:50:17 +0200

    Update CollisionResult inversion to use category-based responses

--- GamePhysicsProj/Source/Core/Private/Physics/CollisionShapeInterface.cpp
+++ GamePhysicsProj/Source/Core/Private/Physics/CollisionShapeInterface.cpp
@@ -1,10 +1,17 @@
 #include "Physics/CollisionShapeInterface.h"
 
+#include "Physics/CollisionObject.h"
+
 CollisionResult CollisionResult::getInverted(CollisionObject* other) const
 {
     CollisionResult invert = *this;
+
+    CollisionObject* oldObject = invert.collisionObject;
+    const CollisionResponse response = oldObject ? oldObject->getCollisionResponseForCategory(other->getCollisionCategory()) : CollisionResponse::Ignore;
+    invert.bCollided = CollisionResponse::Ignore != response;
+    invert.bBlocked = CollisionResponse::Block == response;
     invert.collisionObject = other;
-    invert.collisionNormal *= -1;
+    invert.collisionNormal *= -1 * invert.bCollided;
 
     return invert;
 }
