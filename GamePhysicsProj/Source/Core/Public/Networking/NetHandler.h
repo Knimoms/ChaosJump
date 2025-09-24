b1da72c8c853209151be96327cd59e18e8111c38
Timon <timon-albrecht@outlook.de>
2025-09-24 18:12:44 +0200

    Setup NetHandler base and integrate with TickableInterface

--- GamePhysicsProj/Source/Core/Public/Networking/NetHandler.h
+++ GamePhysicsProj/Source/Core/Public/Networking/NetHandler.h
@@ -1,14 +1,40 @@
 ﻿#pragma once
+#include <map>
 
-class NetHandler
+#include "Base/TickableInterface.h"
+#include "SteamSDK/public/steam/steam_api.h"
+
+class NetHandler : public TickableInterface
 {
+
+private:
+
+    const int mVirtualPort = 1;
+    
+    HSteamListenSocket mListenSocket;
+    HSteamNetPollGroup mPollGroup;
+
+    std::map<uint64_t, HSteamNetConnection> mConnectionMap = {};
+
+    bool bHosting = false;
+    bool bConnectedAsClient = false;
+
+protected:
+
+    STEAM_CALLBACK_MANUAL(NetHandler, handleConnStatusChanged, SteamNetConnectionStatusChangedCallback_t, mCallbackConnStatusChanged);
+    STEAM_CALLBACK(NetHandler, handleGameLobbyJoinRequested, GameLobbyJoinRequested_t, m_GameLobbyJoinRequested);
+    STEAM_CALLBACK(NetHandler, handleGameRichPresenceJoinRequested, GameRichPresenceJoinRequested_t, m_GameRichPresenceJoinRequested);
     
 public:
 
     NetHandler();
     virtual ~NetHandler() = default;
 
-    void HostLocal();
-    void ConnectLocalInstance();
+    void host();
+    void connect();
+
+    //~ Begin TickableInterface
+    void tick(float deltaTime) override;
+    //~ End TickableInterface
     
 };
