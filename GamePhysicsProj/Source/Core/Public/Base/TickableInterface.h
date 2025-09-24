619f5aab6ae0613b6fb54372581ce0a2ead41620
Timon <timon-albrecht@outlook.de>
2025-09-24 18:12:44 +0200

    Add mTickOrder to TickableInterface for controlled ticking

--- GamePhysicsProj/Source/Core/Public/Base/TickableInterface.h
+++ GamePhysicsProj/Source/Core/Public/Base/TickableInterface.h
@@ -7,6 +7,8 @@ class TickableInterface
 
 private:
 
+    uint8_t mTickOrder = UINT8_MAX;
+
     static bool bTickInProgress;
     static std::stack<TickableInterface*> sTickablesCreatedInTick;
     static std::vector<TickableInterface*> sTickables;
